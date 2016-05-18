1c3a94e0f450e93e9866f4abb7e948887ede3516
Gerard Arguelles <gerard.arguelles@live.com>
2016-05-18 23:03:50 +0800

    [wip][spec] session services

---
+++ api/controllers/sessions-controller.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let express = require('express');
+let sessionsController = express.Router();
+let SessionService = require('../services/session-service');
+
+/*
+ * GET /me
+ */
+sessionsController.get('/', function (request, response) {
+  let sessionService = new SessionService(request.decoded);
+  sessionService.getUserData((err, result) => {
+    if (err) {
+      console.log('error', err);
+      response.status(500).json({ error: 'something went wrong' });
+    }
+    else {
+      response.status(200).json(result);
+    }
+  });
+});
+
+/*
+ * GET /me/shouts
+ */
+sessionsController.get('/shouts', function (request, response) {
+  let sessionService = new SessionService(request.decoded);
+  sessionService.shouts((err, documents) => {
+    if (err) {
+      response.status(500).json({ error: 'something went wrong' });
+    } else {
+      response.status(200).json(documents);
+    }
+  });
+});
+
+module.exports = sessionsController;
--- api/middlewares/token-verifier.js
+++ api/middlewares/token-verifier.js
@@ -3,24 +3,16 @@
 var jwt = require('jsonwebtoken');
 
 module.exports = function(request, response, next) {
-
   // check token location
   let token = request.body.token || request.query.token || request.headers['x-access-token']
-
   if (token) {
-
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-
       if (err)
         response.status(403).json({ errorMessage: 'Invalid Token' });
-
       request.decoded = decoded;
       next();
-
     });
-
   } else {
     response.status(403).json({ errorMessage: 'Invalid Token' });
   };
-
 };
