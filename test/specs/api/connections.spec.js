f9dad381177c0955359435262dd156798b763e98
Gerard Arguelles <gerard.arguelles@live.com>
2016-05-19 16:10:08 +0800

    [specs] connections, reorganized test structure

---
+++ test/specs/api/connections.spec.js
@@ -0,0 +1,114 @@
+'use strict';
+
+let request = require('supertest');
+let should = require('should');
+let tokenHelper = require('../../helpers/token');
+let User = require('../../../api/models/user');
+
+describe('Connections', function () {
+
+  let server = {};
+  let token = '';
+  let shoutId = '';
+
+  beforeEach(function () {
+    delete require.cache[require.resolve('../../../bin/www')];
+    server = require('../../../bin/www');
+  });
+
+  afterEach(function (done) {
+    server.close(done)
+  });
+
+  before(function (done) {
+    tokenHelper.createAccessToken(function (err, res) {
+      if (err) throw new Error(err);
+      token = res;
+      done();
+    });
+  });
+
+  /*
+  * Get Followers
+  */
+  it('responds to GET /connections/followers', function (done) {
+    request(server)
+    .get('/connections/following')
+    .set('Content-Type', 'application/json')
+    .set('x-access-token', token)
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .expect(function (res) {
+      res.body.should.be.an.Array;
+    })
+    .end(function (err, response) {
+      if (err) return done(err)
+      done();
+    });
+  });
+
+  /*
+  * Get Following
+  */
+  it('responds to GET /connections/followers', function (done) {
+    request(server)
+    .get('/connections/following')
+    .set('Content-Type', 'application/json')
+    .set('x-access-token', token)
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .expect(function (res) {
+      res.body.should.be.an.Array;
+    })
+    .end(function (err, response) {
+      if (err) return done(err)
+      done();
+    });
+  });
+
+  /*
+  * follow request
+  */
+  it('responds to POST /connections/following', function (done) {
+    request(server)
+    .post('/connections/following')
+    .set('Content-Type', 'application/json')
+    .set('x-access-token', token)
+    .send({
+      username: 'anotheruser'
+    })
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .expect(function (res) {
+      res.body.should.be.an.Object;
+      res.body.should.have.properties(['firstName', 'lastName', 'username']);
+    })
+    .end(function (err, response) {
+      if (err) return done(err)
+      done();
+    });
+  });
+
+  /*
+  * unfollow
+  */
+  it('responds to PUT /connections/following', function (done) {
+    request(server)
+    .put('/connections/following')
+    .set('Content-Type', 'application/json')
+    .set('x-access-token', token)
+    .send({
+      username: 'anotheruser'
+    })
+    .expect(204)
+    .expect('Content-Type', /json/)
+    .expect(function (res) {
+      res.body.should.be.an.Object;
+      res.body.should.have.properties(['firstName', 'lastName', 'username']);
+    })
+    .end(function (err, response) {
+      if (err) return done(err)
+      done();
+    });
+  });
+});
--- test/specs/utils/parameters.spec.js
+++ test/specs/utils/parameters.spec.js
@@ -2,11 +2,10 @@
 
 let _ = require('lodash');
 let should = require('should');
-let parameters = require('../../api/utils/parameters');
+let parameters = require('../../../api/utils/parameters');
 let params = { updatedAt: new Date(), body: 'a shout', something: 'not good' };
 
-describe('Parameters', function (done) {
-
+describe('Utils - Parameters', function (done) {
   it('removes un-permitted parameters', (done) => {
     let result = parameters.permit(params, ['body']);
     result.should.not.have.property('something');
@@ -14,5 +13,4 @@ describe('Parameters', function (done) {
     result.should.have.property('body');
     done();
   });
-
-})
+});
